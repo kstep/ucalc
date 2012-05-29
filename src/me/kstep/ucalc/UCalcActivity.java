b313650d6f0a346084fc93ceecd4cd406681e79c
Konstantin Stepanov <me@kstep.me>
2012-05-29 07:36:09 +0300

    UCalc prototype

---
+++ src/me/kstep/ucalc/UCalcActivity.java
@@ -0,0 +1,13 @@
+package me.kstep.ucalc;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class UCalcActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
