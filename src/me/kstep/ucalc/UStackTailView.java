898957a8a2d60ca28fb04262ded4ec6c8e0c83da
Konstantin Stepanov <me@kstep.me>
2012-05-31 16:45:54 +0300

    moved stack tail view into separate class

---
+++ src/me/kstep/ucalc/UStackTailView.java
@@ -0,0 +1,28 @@
+package me.kstep.ucalc;
+
+import android.widget.TextView;
+import android.content.Context;
+import android.view.Gravity;
+import android.util.AttributeSet;
+
+class UStackTailView extends TextView {
+    public UStackTailView(Context context) {
+        super(context);
+        initialize();
+    }
+
+    public UStackTailView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initialize();
+    }
+
+    public UStackTailView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        initialize();
+    }
+
+    private void initialize() {
+        setGravity(Gravity.CENTER_VERTICAL);
+        setText("");
+    }
+}
