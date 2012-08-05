7d40b2b1f64787a0359a23cbd5c85aa544434c41
Konstantin Stepanov <me@kstep.me>
2012-08-05 15:08:53 +0300

    units manager experimental loadUnits(fileName) method
    This method should load units definitions from text file which come
    along with "units" command line application.
    
    See "units" Linux application for details
    (http://www.gnu.org/software/units/units.html).

--- src/me/kstep/ucalc/units/UnitsManager.java
+++ src/me/kstep/ucalc/units/UnitsManager.java
@@ -3,6 +3,9 @@ import java.util.HashMap;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Set;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
 
 class UnitsManager {
     HashMap<String, Unit> units;
@@ -112,6 +115,72 @@ class UnitsManager {
     }
     public double convert(double value, String from, String to) {
         return convert(value, get(from), get(to));
+    }
+
+    public void clear() {
+        units.clear();
+    }
+
+    // Experimental
+    public void loadUnits(String fileName) {
+        try {
+            BufferedReader file = new BufferedReader(new FileReader(fileName));
+
+            String line = null;
+            while ((line = file.readLine()) != null) {
+                // Remove comments and trailing spaces
+                line = line.replaceFirst("\\s*#.+$", "");
+
+                // Skip line continuations for now
+                if (line.startsWith(" ")) continue;
+
+                line = line.trim();
+
+                // We are not interested in empty lines whatsoever
+                if (line.equals("")) continue;
+
+                String[] definition = line.split("\\s+", 2);
+                /*try{
+                System.out.println(definition[1]);
+                System.out.println(definition[1].equals("!"));
+                }catch(ArrayIndexOutOfBoundsException e){}*/
+
+                if (definition.length != 2) {
+                    continue;
+
+                // Skip prefixes
+                } else if (definition[0].endsWith("-")) {
+                    continue;
+
+                // Skip functional definitions for now
+                } else if (definition[0].endsWith(")")) {
+                    continue;
+
+                // Skip table definitions as well
+                } else if (definition[0].endsWith("]")) {
+                    continue;
+
+                // This is a base unit
+                } else if (definition[1].equals("!") || definition[1].equals("!dimensionless")) {
+                    try {
+                        new BaseUnit(definition[0]);
+                    } catch (UnitExistsException e) {}
+
+                // Skip special directives
+                } else if (definition[0].startsWith("!")) {
+                    continue;
+
+                // Alias
+                } else if (exists(definition[1])) {
+                    try {
+                        addAlias(definition[0], definition[1]);
+                    } catch (UnitExistsException e) {}
+                }
+            }
+
+        } catch (IOException e) {
+            return;
+        }
     }
 }
 
