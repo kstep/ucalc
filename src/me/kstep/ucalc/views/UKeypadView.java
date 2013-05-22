726a72beecf6bc84bdb7f58798ea23dfbed3fe9f
Konstantin Stepanov <me@kstep.me>
2013-05-22 20:54:27 +0300

    fix and simplify keypad layout

--- src/me/kstep/ucalc/views/UKeypadView.java
+++ src/me/kstep/ucalc/views/UKeypadView.java
@@ -3,13 +3,13 @@ package me.kstep.ucalc.views;
 import android.content.Context;
 import android.util.AttributeSet;
 
-import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 
 import me.kstep.ucalc.R;
 
-public class UKeypadView extends FrameLayout {
+public class UKeypadView extends LinearLayout {
     public UKeypadView(Context ctx) {
         super(ctx);
         initialize(ctx);
@@ -21,7 +21,7 @@ public class UKeypadView extends FrameLayout {
     }
 
     private void initialize(Context ctx) {
-        LayoutInflater.from(ctx).inflate(R.layout.main_keypad, this);
+        LayoutInflater.from(ctx).inflate(R.layout.main_keypad, this, true);
     }
 }
 
