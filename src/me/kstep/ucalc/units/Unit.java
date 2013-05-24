497550e1e8436487d6394406b772cd48b0fe5b9e
Konstantin Stepanov <me@kstep.me>
2013-05-24 14:07:11 +0300

    use more robust .equals() compare for units arithmetics

--- src/me/kstep/ucalc/units/Unit.java
+++ src/me/kstep/ucalc/units/Unit.java
@@ -116,14 +116,14 @@ public abstract class Unit {
     // Multiple this by other
     public Unit mul(Unit other) {
         if (other == NONE) return this;
-        if (this == other) return new PowerUnit(this, 2).simplify();
+        if (this.equals(other)) return new PowerUnit(this, 2).simplify();
         return new MultipleUnit(this, other).simplify();
     }
 
     // Divide this by other
     public Unit div(Unit other) {
         if (other == NONE) return this;
-        if (this == other) return NONE;
+        if (this.equals(other)) return NONE;
         return new MultipleUnit(this, new PowerUnit(other, -1)).simplify();
     }
 
