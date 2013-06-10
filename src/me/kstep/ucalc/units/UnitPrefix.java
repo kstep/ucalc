e75c20f038c8d37a036f3e2a5a36e6021e8802d5
Konstantin Stepanov <me@kstep.me>
2013-06-10 06:37:14 +0300

    sort unit prefixes by exponent

--- src/me/kstep/ucalc/units/UnitPrefix.java
+++ src/me/kstep/ucalc/units/UnitPrefix.java
@@ -27,13 +27,13 @@ public class UnitPrefix extends LinearUnit {
      * usually used in computer science.
      */
     final static public double Ki = 1024.0;
-    final static public double Mi = 1024*1024.0;
-    final static public double Gi = 1024*1024*1024.0;
-    final static public double Ti = 1024*1024*1024*1024.0;
-    final static public double Pi = 1024*1024*1024*1024*1024.0;
-    final static public double Ei = 1024*1024*1024*1024*1024*1024.0;
-    final static public double Zi = 1024*1024*1024*1024*1024*1024*1024.0;
-    final static public double Yi = 1024*1024*1024*1024*1024*1024*1024*1024.0;
+    final static public double Mi = 1024.0*1024;
+    final static public double Gi = 1024.0*1024*1024;
+    final static public double Ti = 1024.0*1024*1024*1024;
+    final static public double Pi = 1024.0*1024*1024*1024*1024;
+    final static public double Ei = 1024.0*1024*1024*1024*1024*1024;
+    final static public double Zi = 1024.0*1024*1024*1024*1024*1024*1024;
+    final static public double Yi = 1024.0*1024*1024*1024*1024*1024*1024*1024;
 
     /**
      * Now to the prefixes for negative powers of 10.
@@ -82,7 +82,21 @@ public class UnitPrefix extends LinearUnit {
 		String[] result = new String[fields.length];
 
 		for (int i = 0; i < fields.length; i++) {
-			result[i] = fields[i].getName();
+			try {
+				double v = fields[i].getDouble(null);
+				String n = fields[i].getName();
+
+				int j = (int) Math.round(Math.log10(v));
+				if (-3 > j || j > 3) {
+					j = j / 3 + (j < 0? -2: 2);
+				}
+				j += j < 0? 10: (n.length() == 2 && n.charAt(1) == 'i'? 17: 9);
+
+				//android.util.Log.d("unitprefix", fields[i].getName()+" goes to "+j+" ("+((long)v%1024)+")");
+			    result[j] = n;
+
+			} catch (IllegalAccessException e) {
+			} catch (IllegalArgumentException e) {}
 		}
 
 		return result;
