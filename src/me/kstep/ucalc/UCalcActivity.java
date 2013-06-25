8f828c1a2c95ca6ee777236c79a34743cf0a24fe
Konstantin Stepanov <me@kstep.me>
2013-06-25 05:04:46 +0300

    run garbage collector after operation apply

--- src/me/kstep/ucalc/UCalcActivity.java
+++ src/me/kstep/ucalc/UCalcActivity.java
@@ -383,6 +383,7 @@ public class UCalcActivity extends Activity implements SharedPreferences.OnShare
         } else {
             try {
                 op.apply(state, stack);
+                System.gc();
 
             } catch (UCalcException e) {
                 showToast(e.getMessage());
