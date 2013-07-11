885457fe971f8b4f7ce962bc3f5042f3db8c687c
Konstantin Stepanov <me@kstep.me>
2013-07-11 15:30:13 +0300

    use fields for prefix storage again

--- src/me/kstep/ucalc/units/UnitPrefix.java
+++ src/me/kstep/ucalc/units/UnitPrefix.java
@@ -25,56 +25,64 @@ public class UnitPrefix extends LinearUnit {
             this.fullname = fullname;
         }
 
-        Unit asUnit() {
+        public Unit asUnit() {
             Unit unit = new UnitPrefix(name + "-", name, Unit.NONE);
             unit.fullname = fullname;
             unit.category = Unit.Category.PREFIX;
             return unit;
         }
+
+        public double doubleValue() {
+            return scale;
+        }
+
+        public String toString() {
+            return name;
+        }
     }
 
-    final static Prefix[] PREFIXES = new Prefix[]{
-        /**
-         * Prefixes for negative powers of 10.
-         */
-        new Prefix(1e-24, "y", "yocto"),
-        new Prefix(1e-21, "z", "zepto"),
-        new Prefix(1e-18, "a", "atto"),
-        new Prefix(1e-15, "f", "femto"),
-        new Prefix(1e-12, "p", "pico"),
-        new Prefix(1e-9, "n", "nano"),
-        new Prefix(1e-6, "μ", "micro"),
-        new Prefix(1e-3, "m", "milli"),
-        new Prefix(1e-2, "c", "centi"),
-        new Prefix(1e-1, "d", "deci"),
+    final static int TOTAL_PREFIXES_NUMBER = 28;
 
-        /**
-         * Here go prefixes, which define positive powers of 10.
-         */
-        new Prefix(10, "da", "deca"),
-        new Prefix(100, "h", "hecto"),
-        new Prefix(1000, "k", "kilo"),
-        new Prefix(1e6, "M", "Mega"),
-        new Prefix(1e9, "G", "Giga"),
-        new Prefix(1e12, "T", "Tera"),
-        new Prefix(1e15, "P", "Peta"),
-        new Prefix(1e18, "E", "Exa"),
-        new Prefix(1e21, "Z", "Zetta"),
-        new Prefix(1e24, "Y", "Yotta"),
+    /**
+     * Prefixes for negative powers of 10.
+     */
+    final static public Prefix y = new Prefix(1e-24, "y", "yocto");
+    final static public Prefix z = new Prefix(1e-21, "z", "zepto");
+    final static public Prefix a = new Prefix(1e-18, "a", "atto");
+    final static public Prefix f = new Prefix(1e-15, "f", "femto");
+    final static public Prefix p = new Prefix(1e-12, "p", "pico");
+    final static public Prefix n = new Prefix(1e-9, "n", "nano");
+    final static public Prefix μ = new Prefix(1e-6, "μ", "micro");
+    final static public Prefix m = new Prefix(1e-3, "m", "milli");
+    final static public Prefix c = new Prefix(1e-2, "c", "centi");
+    final static public Prefix d = new Prefix(1e-1, "d", "deci");
 
-        /**
-         * These are relatively new prefixes for positive powers of 2,
-         * usually used in computer science.
-         */
-        new Prefix(1024.0, "Ki", "Kibi"),
-        new Prefix(1024.0*1024, "Mi", "Mibi"),
-        new Prefix(1024.0*1024*1024, "Gi", "Gibi"),
-        new Prefix(1024.0*1024*1024*1024, "Ti", "Tibi"),
-        new Prefix(1024.0*1024*1024*1024*1024, "Pi", "Pibi"),
-        new Prefix(1024.0*1024*1024*1024*1024*1024, "Ei", "Ebi"),
-        new Prefix(1024.0*1024*1024*1024*1024*1024*1024, "Zi", "Zibi"),
-        new Prefix(1024.0*1024*1024*1024*1024*1024*1024*1024, "Yi", "Yibi"),
-    };
+    /**
+     * Here go prefixes, which define positive powers of 10.
+     */
+    final static public Prefix da = new Prefix(10, "da", "deca");
+    final static public Prefix h = new Prefix(100, "h", "hecto");
+    final static public Prefix k = new Prefix(1000, "k", "kilo");
+    final static public Prefix M = new Prefix(1e6, "M", "Mega");
+    final static public Prefix G = new Prefix(1e9, "G", "Giga");
+    final static public Prefix T = new Prefix(1e12, "T", "Tera");
+    final static public Prefix P = new Prefix(1e15, "P", "Peta");
+    final static public Prefix E = new Prefix(1e18, "E", "Exa");
+    final static public Prefix Z = new Prefix(1e21, "Z", "Zetta");
+    final static public Prefix Y = new Prefix(1e24, "Y", "Yotta");
+
+    /**
+     * These are relatively new prefixes for positive powers of 2;
+     * usually used in computer science.
+     */
+    final static public Prefix Ki = new Prefix(1024.0, "Ki", "Kibi");
+    final static public Prefix Mi = new Prefix(1024.0*1024, "Mi", "Mibi");
+    final static public Prefix Gi = new Prefix(1024.0*1024*1024, "Gi", "Gibi");
+    final static public Prefix Ti = new Prefix(1024.0*1024*1024*1024, "Ti", "Tibi");
+    final static public Prefix Pi = new Prefix(1024.0*1024*1024*1024*1024, "Pi", "Pibi");
+    final static public Prefix Ei = new Prefix(1024.0*1024*1024*1024*1024*1024, "Ei", "Ebi");
+    final static public Prefix Zi = new Prefix(1024.0*1024*1024*1024*1024*1024*1024, "Zi", "Zibi");
+    final static public Prefix Yi = new Prefix(1024.0*1024*1024*1024*1024*1024*1024*1024, "Yi", "Yibi");
 
     /**
      * We override constructor to define prefixed units conviniently.
@@ -87,21 +95,45 @@ public class UnitPrefix extends LinearUnit {
         super(name, prefixToScale(prefix), targetUnit);
     }
 
+    /**
+     * This method is a little magical. It uses introspection to lookup
+     * prefix value in one of class constants above.
+     */
     public static double prefixToScale(String prefix) {
-        for (Prefix p : PREFIXES) {
-            if (p.name.equals(prefix)) {
-                return p.scale;
-            }
+        try {
+            Field property = UnitPrefix.class.getDeclaredField(prefix);
+            return ((Prefix) property.get(null)).doubleValue();
+
+        } catch (ClassCastException e) {
+        } catch (IllegalAccessException e) {
+        } catch (NoSuchFieldException e) {
         }
 
         return 0.0;
     }
 
     public static Unit[] getPrefixes() {
-        Unit[] prefixes = new Unit[PREFIXES.length];
+        Field[] fields = UnitPrefix.class.getDeclaredFields();
+        Unit[] prefixes = new Unit[TOTAL_PREFIXES_NUMBER];
 
-        for (int i = 0; i < prefixes.length; i++) {
-            prefixes[i] = PREFIXES[i].asUnit();
+        for (int i = 0; i < fields.length; i++) {
+            try {
+                Prefix p = (Prefix) fields[i].get(null);
+                String n = p.name;
+
+                int j = (int) Math.round(Math.log10(p.doubleValue()));
+                if (-3 > j || j > 3) {
+                    j = j / 3 + (j < 0 ? -2: 2);
+                }
+                j += j < 0 ? 10: (n.length() == 2 && n.charAt(1) == 'i' ? 17: 9);
+
+                prefixes[j] = p.asUnit();
+
+            } catch (IllegalAccessException e) {
+            } catch (IllegalArgumentException e) {
+            } catch (ArithmeticException e) {
+            } catch (ClassCastException e) {
+            }
         }
 
         return prefixes;
