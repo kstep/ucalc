1f768f273864b95bde26fbd703c5d17cb10f6924
Konstantin Stepanov <me@kstep.me>
2013-07-13 01:32:13 +0300

    trigonometric functions for complex numbers

--- src/me/kstep/ucalc/operations/UMath.java
+++ src/me/kstep/ucalc/operations/UMath.java
@@ -6,6 +6,7 @@ import me.kstep.ucalc.numbers.UInteger;
 import me.kstep.ucalc.units.UnitNum;
 import me.kstep.ucalc.units.Unit;
 import me.kstep.ucalc.units.UnitsManager;
+import me.kstep.ucalc.numbers.UComplex;
 
 final public class UMath {
     final private static Unit radian = UnitsManager.getInstance().get("rad");
@@ -13,21 +14,52 @@ final public class UMath {
     final public static UNumber PI = new UFloat(Math.PI);
     final public static UNumber E = new UFloat(Math.E);
 
-    public static double angle(UNumber value) {
+    public static UNumber angle(UNumber value) {
         if (value instanceof UnitNum) {
             UnitNum num = (UnitNum) value;
-            return (num.unit == radian? num: num.convert(radian)).value.doubleValue();
+            return (num.unit == radian? num: num.convert(radian)).value;
         } else {
-            return value.doubleValue();
+            return value;
         }
     }
 
+	// \cos(a+bi) = \cos a \cosh b - i\sin a \sinh b
+	// \sin(a+bi) = \sin a \cosh b + i\cos a \sinh b
+	// \cosh(a+bi) = \cosh a \cos b + i\sinh a \sin b
+	// \sinh(a+bi) = \sinh a \cos b + i\cosh a \sin b
+	// \cos^{-1} x = -i\ln\left[x\pm i\sqrt{1-x^2}\right]
+	// \sin^{-1} x = -i\ln\left[ix\pm \sqrt{1-x^2}\right]
+    // \tan^{-1} x = \frac{i}{2} \ln\frac{i+x}{i-x}
+	// \cosh^{-1} x = \ln\left[x\pm \sqrt{x^2-1}\right]
+	// \sinh^{-1} x = \ln\left[x\pm \sqrt{x^2+1}\right]
+    // \ln re^{iP} = \ln r + iP + 2k\pi i
+	// \ln (-1) = \ln \left(1e^{i\pi}\right) = \pi i
     public static UNumber sin(UNumber value) {
-        return new UFloat(Math.sin(angle(value)));
+		UNumber phi = angle(value);
+		if (phi instanceof UComplex) {
+			UComplex teta = (UComplex) phi;
+            return new UComplex(
+			    Math.sin(teta.real.doubleValue()) *
+				Math.cosh(teta.imag.doubleValue()),
+				Math.cos(teta.real.doubleValue()) *
+				Math.sinh(teta.imag.doubleValue())
+			);
+		}
+        return new UFloat(Math.sin(phi.doubleValue()));
     }
 
     public static UNumber cos(UNumber value) {
-        return new UFloat(Math.cos(angle(value)));
+		UNumber phi = angle(value);
+		if (phi instanceof UComplex) {
+			UComplex teta = (UComplex) phi;
+            return new UComplex(
+			    Math.cos(teta.real.doubleValue()) *
+				Math.cosh(teta.imag.doubleValue()),
+				Math.sin(teta.real.doubleValue()) *
+				Math.sinh(teta.imag.doubleValue())
+			);
+		}
+        return new UFloat(Math.cos(phi.doubleValue()));
     }
 
     public static UNumber acos(UNumber value, Unit unit) {
@@ -45,7 +77,11 @@ final public class UMath {
     }
 
     public static UNumber tan(UNumber value) {
-        return new UFloat(Math.tan(angle(value)));
+		UNumber phi = angle(value);
+		if (phi instanceof UComplex) {
+			return sin(phi).div(cos(phi));
+		}
+        return new UFloat(Math.tan(phi.doubleValue()));
     }
 
     public static UNumber atan(UNumber value, Unit unit) {
