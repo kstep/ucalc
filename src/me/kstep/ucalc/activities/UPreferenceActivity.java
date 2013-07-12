d42e75ff20f66512d8fae4f64f2d70668dd79b90
Konstantin Stepanov <me@kstep.me>
2013-07-12 16:02:33 +0300

    use preference fragment instead of deprecated PreferenceActivity.addPreferencesFromResource() method

--- src/me/kstep/ucalc/activities/UPreferenceActivity.java
+++ src/me/kstep/ucalc/activities/UPreferenceActivity.java
@@ -1,14 +1,22 @@
 package me.kstep.ucalc.activities;
 
 import android.app.ActionBar;
+import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
 import android.view.MenuItem;
-import android.content.Intent;
-
 import me.kstep.ucalc.R;
 
 public class UPreferenceActivity extends PreferenceActivity {
+
+    public static class UPreferenceFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.preferences);
+        }
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -17,7 +25,9 @@ public class UPreferenceActivity extends PreferenceActivity {
 
         super.onCreate(savedInstanceState);
 
-        addPreferencesFromResource(R.xml.preferences);
+        getFragmentManager().beginTransaction()
+            .replace(android.R.id.content, new UPreferenceFragment())
+            .commit();
 
         ActionBar ab = getActionBar();
         ab.setHomeButtonEnabled(true);
