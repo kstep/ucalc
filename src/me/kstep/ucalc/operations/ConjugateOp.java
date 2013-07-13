320610baf0a0ff2f4c447ab2b2c8f5073f75584d
Konstantin Stepanov <me@kstep.me>
2013-07-13 22:07:12 +0300

    conjugate and decompose operations work with unit numbers

--- src/me/kstep/ucalc/operations/ConjugateOp.java
+++ src/me/kstep/ucalc/operations/ConjugateOp.java
@@ -2,13 +2,24 @@ package me.kstep.ucalc.operations;
 import me.kstep.ucalc.collections.UState;
 import me.kstep.ucalc.collections.UStack;
 import me.kstep.ucalc.numbers.UComplex;
+import me.kstep.ucalc.numbers.UNumber;
+import me.kstep.ucalc.units.UnitNum;
 
 public class ConjugateOp extends UOperation {
 
 	public void apply(UState state, UStack stack) {
-		if (stack.peek() instanceof UComplex) {
-			stack.push(((UComplex) stack.pop()).conj());
+		UNumber value = stack.pop();
+		if (value instanceof UComplex) {
+			value = ((UComplex) value).conj();
+
+		} else if (value instanceof UnitNum) {
+			if (((UnitNum) value).value instanceof UComplex) {
+				value = new UnitNum(
+				    ((UComplex) ((UnitNum) value).value).conj(),
+					((UnitNum) value).unit);
+			}
 		}
+		stack.push(value);
 	}
 
 	public int arity() {
