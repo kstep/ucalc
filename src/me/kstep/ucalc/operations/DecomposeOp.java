01f9355453f49a1f5f58d946cf3f37b5d508c1c8
Konstantin Stepanov <me@kstep.me>
2013-07-13 14:04:55 +0300

    decompose imaginary part as is

--- src/me/kstep/ucalc/operations/DecomposeOp.java
+++ src/me/kstep/ucalc/operations/DecomposeOp.java
@@ -2,13 +2,14 @@ package me.kstep.ucalc.operations;
 import me.kstep.ucalc.collections.UState;
 import me.kstep.ucalc.collections.UStack;
 import me.kstep.ucalc.numbers.UComplex;
+import me.kstep.ucalc.numbers.UNumber;
 
 public class DecomposeOp extends UOperation {
 
 	public void apply(UState state, UStack stack) {
 		if (stack.peek() instanceof UComplex) {
 			UComplex value = (UComplex) stack.pop();
-			stack.push(value.imag);
+			stack.push(new UComplex(UNumber.ZERO, value.imag));
 			stack.push(value.real);
 		}
 	}
