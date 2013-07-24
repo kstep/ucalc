84977d7b4cc56161af805cddf06e0919b51de9e8
Konstantin Stepanov <me@kstep.me>
2013-07-24 15:03:57 +0300

    reformat code

--- src/me/kstep/ucalc/activities/UPreferenceActivity.java
+++ src/me/kstep/ucalc/activities/UPreferenceActivity.java
@@ -1,15 +1,15 @@
 package me.kstep.ucalc.activities;
 
 import android.app.ActionBar;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
 import android.view.MenuItem;
 import me.kstep.ucalc.R;
-import android.content.pm.PackageManager;
-import android.preference.Preference;
-import android.app.Activity;
 
 public class UPreferenceActivity extends PreferenceActivity {
 
@@ -18,18 +18,18 @@ public class UPreferenceActivity extends PreferenceActivity {
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.preferences);
-			Preference p = findPreference("version");
-			p.setSummary(String.format(p.getSummary().toString(), getVersionName()));
+            Preference p = findPreference("version");
+            p.setSummary(String.format(p.getSummary().toString(), getVersionName()));
         }
-		
-		public String getVersionName() {
-			try {
-				Activity activity = getActivity();
-				return activity.getPackageManager().getPackageInfo(activity.getPackageName(), 0).versionName;
-			} catch (PackageManager.NameNotFoundException e) {
-				return "";
-			}
-		}
+
+        public String getVersionName() {
+            try {
+                Activity activity = getActivity();
+                return activity.getPackageManager().getPackageInfo(activity.getPackageName(), 0).versionName;
+            } catch (PackageManager.NameNotFoundException e) {
+                return "";
+            }
+        }
     }
 
     @Override
